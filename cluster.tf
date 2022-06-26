4bdad9a1ba6cef1faa429e884ba13e14f86f78a9
Aurélien GARNIER <au2001sfg@gmail.com>
2022-06-26 15:55:11 +0200

    Make service account cluster admin

--- cluster.tf
+++ cluster.tf
@@ -7,6 +7,25 @@ resource "google_container_cluster" "prod_cluster" {
 }
 
 data "google_client_config" "provider" {}
+data "google_client_openid_userinfo" "me" {}
+
+resource "kubernetes_cluster_role_binding" "cluster_admin" {
+  metadata {
+    name = "google-service-account-admin"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  subject {
+    kind      = "User"
+    api_group = "rbac.authorization.k8s.io"
+    name      = data.google_client_openid_userinfo.me.email
+  }
+}
 
 locals {
   cluster_host           = "https://${google_container_cluster.prod_cluster.endpoint}"
