d1b109149123e1e52505a79e995cd662da30545d
Aurélien GARNIER <au2001sfg@gmail.com>
2022-06-27 15:16:35 +0200

    Create SSL certificate

--- apps/ingress.tf
+++ apps/ingress.tf
@@ -1,3 +1,19 @@
+resource "kubernetes_manifest" "app_ssl_certificate" {
+  manifest = {
+    apiVersion = "networking.gke.io/v1"
+    kind       = "ManagedCertificate"
+
+    metadata = {
+      name      = "app-ssl-certificate"
+      namespace = kubernetes_namespace.namespace.metadata[0].name
+    }
+
+    spec = {
+      domains = var.domains
+    }
+  }
+}
+
 resource "kubernetes_ingress_v1" "app_ingress" {
   metadata {
     name      = "app-ingress"
@@ -10,7 +26,8 @@ resource "kubernetes_ingress_v1" "app_ingress" {
     }
 
     annotations = {
-      "kubernetes.io/ingress.class" = "gce"
+      "kubernetes.io/ingress.class"            = "gce"
+      "networking.gke.io/managed-certificates" = kubernetes_manifest.app_ssl_certificate.manifest.metadata.name
     }
   }
 
